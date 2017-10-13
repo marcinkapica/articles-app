3984d1b7d3ba88eac6f89fbd8576f318b9dcfd96
martions <m.kapica@agencjakrecisie.pl>
2017-10-13 22:03:16 +0200

    adding button and scripts

---
+++ app/js/main.js
@@ -0,0 +1,40 @@
+$(function() {
+
+    var loadButton = $('#loadButton');
+    var postList = $('#postList');
+    $('#postList').hide();
+    // $('#loader').hide();
+
+    loadButton.on('click', function() {
+        $('#loader').show();
+        $("#loadButton").html('Loading ...');
+        $("#loadButton").addClass('lol');
+
+        var lol = $.ajax({
+            dataType: "json",
+            url: 'https://www.future-processing.pl/blog/wp-json/wp/v2/posts',
+            type: 'GET',
+            success: function(posts) {
+                $.each(posts, function(index, post) {
+                    var titleHTML = '<h1 class="title">'+post.title.rendered+'</h1>';
+                    var date = moment(post.date).format('DD-MM-YYYY');
+                    var dateHTML = '<time class="date">Posted on '+date+'</time>';
+                    var excerptHTML = '<span class="text">'+post.excerpt.rendered+'</span>';
+                    var readMoreHTML = '<a href="'+post.link+'" class="link">Read more <i class="fa fa-angle-right" aria-hidden="true"></i></a>';
+                    postList.append('<li class="list__item">'+titleHTML+'<div class="content">'+dateHTML+excerptHTML+readMoreHTML+'</div><hr></li>');
+                })
+            }
+        });
+
+        setTimeout(function() {
+            lol.then(function(){
+                $('#loader').hide();
+                $('#loadButton').hide();
+                $('#postList').show();
+            });
+        }, 3000);
+
+    });
+
+});
+
