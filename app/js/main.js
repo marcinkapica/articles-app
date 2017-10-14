b415a228387c6330e5ec3f2b3d6eafebd6afcfdb
martions <m.kapica@agencjakrecisie.pl>
2017-10-14 22:37:06 +0200

    added template for list item element

--- app/js/main.js
+++ app/js/main.js
@@ -5,6 +5,10 @@ $(function() {
     var loader = $('#loader');
     postList.hide();
 
+    Handlebars.registerHelper('foo', function(date) {
+        return  moment(date).format('DD.MM.YYYY');
+    });
+
     loadButton.on('click', function() {
         loader.show();
         loadButton.html('Loading ...');
@@ -14,15 +18,12 @@ $(function() {
             dataType: "json",
             url: 'https://www.future-processing.pl/blog/wp-json/wp/v2/posts',
             type: 'GET',
-            success: function(posts) {
-                $.each(posts, function(index, post) {
-                    var titleHTML = '<h1 class="title">'+post.title.rendered+'</h1>';
-                    var date = moment(post.date).format('DD.MM.YYYY');
-                    var dateHTML = '<div class="dateContainer"><time class="date">'+date+'</time></div>';
-                    var excerptHTML = '<span class="text">'+post.excerpt.rendered+'</span>';
-                    var readMoreHTML = '<a href="'+post.link+'" class="link">Read more <i class="fa fa-angle-right" aria-hidden="true"></i></a>';
-                    postList.append('<li class="list__item row"><div class="col-xs-12 col-md-2">'+dateHTML+'</div><div class="col-xs-12 col-md-10"><div class="content">'+titleHTML+excerptHTML+readMoreHTML+'</div></div></li>');
-                })
+            success: function(data) {
+                var template = Handlebars.compile($('#listTemplate').html());
+                var dataObject = {
+                    "posts": data
+                };
+                postList.html(template(dataObject));
             }
         });
 
