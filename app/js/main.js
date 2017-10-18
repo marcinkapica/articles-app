30603fa511e2198c454fb833437d86f781792e0c
marcinkapica <marcinkapica@outlook.com>
2017-10-18 16:11:23 +0200

    adding loading and list displaying actions to functions

--- app/js/main.js
+++ app/js/main.js
@@ -4,32 +4,40 @@ $(document).ready(function() {
   var button = $('#button');
   var list = $('#list');
   var loader = $('#loader');
-  list.hide();
+  var getData = $.ajax({
+    dataType: 'json',
+    url: 'https://www.future-processing.pl/blog/wp-json/wp/v2/posts',
+    type: 'GET',
+    success: function(data) {
+      var template = Handlebars.compile($('#list-item-template').html());
+      list.html(template(data));
+    }
+  });
+
+  function showLoader() {
+    loader.show();
+    button.html('Loading ...');
+    button.addClass('button-downward');
+  }
+
+  function showList() {
+    loader.hide();
+    button.hide();
+    list.fadeIn();
+  }
 
   Handlebars.registerHelper('parseDate', function(date) {
     return moment(date).format('DD.MM.YYYY');
   });
 
-  button.on('click', function() {
-    var getData = $.ajax({
-      dataType: 'json',
-      url: 'https://www.future-processing.pl/blog/wp-json/wp/v2/posts',
-      type: 'GET',
-      success: function(data) {
-        var template = Handlebars.compile($('#list-item-template').html());
-        list.html(template(data));
-      }
-    });
+  list.hide();
 
-    loader.show();
-    button.html('Loading ...');
-    button.addClass('button-downward');
+  button.on('click', function() {
+    showLoader();
 
     setTimeout(function() {
       getData.then(function() {
-        loader.hide();
-        button.hide();
-        list.fadeIn();
+        showList();
       });
     }, 3000);
   });
